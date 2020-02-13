3c77498746a199ee4a04ce4e0c33cc99fa55173a
beli302 <belindashirkiz2@gmail.com>
2020-02-13 15:57:35 +0300

    added error.py file in main folder

--- Pitches/app/main/__init__.py
+++ Pitches/app/main/__init__.py
@@ -1,9 +1,5 @@
-from . import main
-from flask import render_template
+from flask import Blueprint
 
-@main.app_errorhandler(404)
-def four_Ow_four(error):
-    """
-    Function to render the 404 error page
-    """
-    return render_template('fourOwfour.html'),404
+main = Blueprint('main', __name__)
+
+from .import errors, views
--- Pitches/app/main/errors.py
+++ Pitches/app/main/errors.py
@@ -1,5 +1,9 @@
-from flask import Blueprint
+from . import main
+from flask import render_template
 
-main = Blueprint('main', __name__)
-
-from .import errors, views
+@main.app_errorhandler(404)
+def four_Ow_four(error):
+    """
+    Function to render the 404 error page
+    """
+    return render_template('fourOwfour.html'),404
