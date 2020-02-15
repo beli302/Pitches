4c811822dadbad3e8dad18a0121adb14837a7973
beli302 <belindashirkiz2@gmail.com>
2020-02-15 16:06:04 +0300

    added templates folder on app folder

--- Pitches/app/auth/views.py
+++ Pitches/app/auth/views.py
@@ -1,41 +1,52 @@
-from wtforms.validators import Required, Email, EqualTo
-from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
-from flask_wtf import FlaskForm
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user
+from .forms import RegistrationForm, LoginForm
+from . import auth
+from ..import db
 from ..models import User
 
 
-class RegistrationForm(FlaskForm):
+# registration route
+@auth.route('templates/auth/reqister',methods=['GET','POST'])
+def register():
     """
-    RegstrationForm class that passes in the required details for validation
+    Function that registers the users
     """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-    email = StringField('your email address', validators=[Required(), Email()])
-    username = StringField('your username', validators=[Required()])
-    password = PasswordField('password', validators=[Required(), EqualTo('password',message='passwords must match')])
-    password_confirm = PasswordField('confirm password', validators=[Required()])
-    submit = SubmitField('sign Up')
+        return redirect(url_for('auth.login'))
 
+    title = "Registration"
 
-    #custom validators
-    def validate_email(self, data_field):
-        """
-        Functions takes in the data field and checks our database to confirm user Validation
-        """
-        if User.query.filter_by(email = data_field.data).first():
-            raise ValidationError('There is an account with that email')
+    return render_template('auth/register.html', registration_form = form, title = title)
 
 
-    def validate_username(self, data_field):
-        """
-        Function checks if the username is unique and raises ValidationError
-        """
-        if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That user name is already in use, please try another one')
+# Login function
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    """
+    Function that checks if the form is validated
+    """
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.d ata)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+        flash('Invalid Username or Password')
+
+    title = "60sec Pitch|Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
 
 
-#login class  takes three inputs from the user
-class LoginForm(FlaskForm):
-    email = StringField('Your email address', validators=[Required(),Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+#logout function
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
